d3a8f3b372908dc20b0b841780db04c002bcd0a4
EL Marsho <emarsho@gmail.com>
2024-07-21 22:41:24 -0500

    2nd commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
+/* eslint-disable react-refresh/only-export-components */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {App} from './App.jsx'
+
+const DATA = [
+  { id:"todo-0", name:"Eat", completed: true },
+  { id:"todo-1", name:"Sleep", completed: false },
+  { id:"todo-2", name:"Pray", completed: false },
+
+]
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA} />
   </React.StrictMode>,
 )
